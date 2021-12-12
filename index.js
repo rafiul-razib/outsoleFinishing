de7f68a0e56b1e6a08601e3ae8b33800bd297a2f
rafiul-razib <rafiul.rzb@gmail.com>
2021-12-12 15:46:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require ("express");
+const cors = require ("cors")
+const { MongoClient } = require('mongodb');
+const fileUpload = require ("express-fileupload");
+const ObjectId = require ('mongodb').ObjectId;
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload())
+
+
+const uri = "mongodb+srv://rafi25:apex22001@cluster0.9gfgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      console.log("database connected")
+      const database = client.db("outsoleFinishing");
+      const recipeCollection = database.collection("recipe");
+      // create a document to insert
+
+    //   POST Recipe
+
+    app.post('/add-recipe', async(req, res)=>{
+        const last = req.body.last;
+        const art = req.body.art;
+        const customer = req.body.customer;
+        const finishing = req.body.finishing;
+        const manpower = req.body.manpower;
+
+        const pic = req.files.image;
+        const picData = pic.data;
+        const encodedPic = picData.toString('base64');
+        const imageBuffer = Buffer.from(encodedPic, 'base64');
+
+        const outsoleRecipe = {
+            last,
+            art,
+            customer,
+            finishing,
+            manpower,
+            imageBuffer
+        }
+
+        const result = await recipeCollection.insertOne(outsoleRecipe);
+        res.json(result)
+    })
+
+    // GET recipe
+
+    app.get('/recipe', async(req, res)=>{
+        const cursor = recipeCollection.find({});
+        const result = await cursor.toArray();
+        res.json(result)
+    })
+      
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send("hello component!!")
+})
+
+app.listen(port, ()=>{
+    console.log("Listening to the port", port)
+})
